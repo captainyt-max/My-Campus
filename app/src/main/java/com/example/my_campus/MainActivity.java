80599eb8117f3c470c3225da66d9b4de2f46a08c
captainyt-max <swdcaptain@gmail.com>
2024-10-26 18:55:23 +0530

    Campus Activity now can be updated by admins, added edit, delete and send new message options. All functions are working properly. Admin button visibility will now be updated on runtime, change in role will trigger the admin section visibility.

--- app/src/main/java/com/example/my_campus/MainActivity.java
+++ app/src/main/java/com/example/my_campus/MainActivity.java
@@ -25,6 +25,7 @@ import androidx.fragment.app.FragmentTransaction;
 import com.chaquo.python.Python;
 import com.chaquo.python.android.AndroidPlatform;
 import com.example.my_campus.Fragments.fragmentAdmin;
+import com.example.my_campus.Fragments.fragmentGuidance;
 import com.example.my_campus.Fragments.fragmentHomepage;
 import com.example.my_campus.Fragments.fragmentHostelInfo;
 import com.example.my_campus.Fragments.fragmentSyllabus;
@@ -173,7 +174,11 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 if (itemId == R.id.navGuidance){
-                    Toast.makeText(MainActivity.this, "Clicked On Guidance", Toast.LENGTH_SHORT).show();
+                    fragmentGuidance fragment_Guidance = new fragmentGuidance();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.mainLayout, fragment_Guidance);
+                    fragmentTransaction.commit();
+                    drawerLayout.closeDrawer(GravityCompat.START);
                 }
 
                 if (itemId == R.id.navHostel){
@@ -216,18 +221,11 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 if (itemId == R.id.navAdmin){
-//                    if(!role[0].equals("admin")){
-//                        Toast.makeText(MainActivity.this, "You are not admin", Toast.LENGTH_SHORT).show();
-//                        drawerLayout.closeDrawer(GravityCompat.START);
-//                    }
-//                    else{
-                        fragmentAdmin fragmentadmin = new fragmentAdmin();
-                        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
-                        fragmentTransaction.replace(R.id.mainLayout, fragmentadmin);
-                        fragmentTransaction.commit();
-                        drawerLayout.closeDrawer(GravityCompat.START);
-//                    }
-
+                    fragmentAdmin fragmentadmin = new fragmentAdmin();
+                    FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.mainLayout, fragmentadmin);
+                    fragmentTransaction.commit();
+                    drawerLayout.closeDrawer(GravityCompat.START);
                 }
                 return false;
             }
